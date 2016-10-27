799d792ff3b5334ff3d5d43e77ef141e1a959935
Hayden Carlson <haydog2@hotmail.com>
2016-10-27 22:43:59 +0000

    Fixes path syntax

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.use(express.static('public'));
 app.get("/", (req, res) => {
 
   res.render("index");
-}
+});
 
-app.post("/" (req, res) => {
+app.post("/", (req, res) => {
 
 
-}
+});
 
 
 
