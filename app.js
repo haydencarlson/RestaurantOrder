14a9cc23525d3a9f0dfceece8237b54d0faea6aa
Hayden Carlson <haydog2@hotmail.com>
2016-10-29 04:00:05 +0000

    1. Updated database 2. Added add to cart route. 3. View cart route

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const settings = require("./settings");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session')
 const app = express();
 const PORT = 8080;
 var knex = require('knex')({
@@ -14,30 +16,78 @@ var knex = require('knex')({
   }
 });
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 
 app.get("/", (req, res) => {
-
- 
+  res.render("index");
 });
 
-app.get("/addtocart", (req, res) => {
-console.log("hello");
-});
 
-app.post("/addtocart", (req, res) => {
-  knex.insert([{food: 'Chicken Parmigiana', price: '17', duration: '1020'}]).into('menu')
-      .then(function(){
-        console.log("Added to cart");
+app.post("/neworder", (req, res) => {
+  let temporaryID = generateRandomString();
+  req.session.orderid = temporaryID;
+
+  knex('order').insert({temporary_id: temporaryID})
+    .returning("id")
+      .then(function(id) {// [7]
+        
+        req.session.orderid = id[0];
+        console.log("Orderid", req.session.orderid);
+
       });
 
+  res.sendStatus(200);
 
 });
 
 
+app.post("/addtocart", (req, res) => {
+  var foodId = Number(req.body.foodId);
+  var orderid = req.session.orderid;
+  console.log("hello", orderid);
+  knex('order_item').insert({menu_item_id: foodId, order_id: orderid}).asCallback(function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log("foodId", foodId);
+  res.sendStatus(200);
+});
+
+
+
+
+app.get("/viewcart", (req, res) => {
+ var ordr = req.session.orderid;
+  knex.select('').from('menu')
+      .join('order_item', 'menu_item_id', 'menu.id').where("order_id", "=", req.session.orderid).asCallback((err, rows) => {
+      
+    if (err) {
+      console.log(err);
+    }
+      console.log(rows);
+  });
+    res.sendStatus(200);
+});
+  
 app.listen(PORT, function() {
   console.log(`Now listening on port ${PORT}`);
 });
+
+const generateRandomString = () => {
+  let randomString = "";
+  var possible = "0123456789";
+for (let i = 0; i < 6; i++)
+    randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+  return randomString;
+};
 
 
 
--- public/script.js
+++ public/script.js
@@ -1,12 +1,30 @@
 $(function() {
 
-  $("#item1").on("click", function(event) {
-
-    $.ajax({
-      method: "post",
-      url: "/addtocart",
-    });
-
+  $(".foods1").on("click", function(event) {
+    var foodId = $(this).attr("data-food");
+    console.log("food id", foodId);
+      $.ajax({
+        method: "post",
+        url: "/addtocart",
+        data: {foodId: foodId }
+      }).fail(function(err) {
+        console.log(err);
+     });
   });
   
+  $("#order-button").on("click", function() {
+
+    console.log("clicked");
+    $.ajax({
+      method: "post",
+      url: "/neworder"
+    });
+  });
+
+  $(".cartButton").on("click", function() {
+    $.ajax({
+      method: "get",
+      url: "/viewcart"
+    });
+  });
 });
